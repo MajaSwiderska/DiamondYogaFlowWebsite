9def62c5cabc694fafec3a7dd0f7e8db878885ca
MajaSwiderska <150272450+MajaSwiderska@users.noreply.github.com>
2026-03-05 12:34:17 -0600

    The base of the website, still in progress

---
+++ script.js
@@ -0,0 +1,13 @@
+function scrollToContact(){
+document.getElementById("contact").scrollIntoView({
+behavior: "smooth"
+});
+}
+
+document.getElementById("contactForm").addEventListener("submit", function(e){
+
+e.preventDefault();
+
+alert("Thank you! Your message has been sent.");
+
+});
